e54fd3df3e5ed770abbc3f5e4bd6ff26e649098e
Nevah5 <neevaah5@gmail.com>
2023-02-04 01:06:39 +0100

    Registered listener

--- src/main/java/dev/nevah5/mc/bungee_plugin/BungeePlugin.java
+++ src/main/java/dev/nevah5/mc/bungee_plugin/BungeePlugin.java
@@ -1,30 +1,22 @@
 package dev.nevah5.mc.bungee_plugin;
 
-import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
-public class BungeePlugin extends Plugin {
+public class BungeePlugin extends Plugin implements Listener {
     @Override
     public void onEnable(){
+        getProxy().getPluginManager().registerListener(this, this);
         getLogger().info("Yay! It loads!");
     }
 
     @EventHandler
-    public void onPostLogin(PostLoginEvent postLoginEvent) {
-        ProxiedPlayer player = postLoginEvent.getPlayer();
-        player.setTabHeader(
-                new TextComponent(String.format("\n         %sᐅ %s%smc.nevah5.dev %sᐊ         \n\n",
-                        ChatColor.GRAY, ChatColor.BLUE,
-                        ChatColor.BOLD, ChatColor.GRAY
-                )),
-                new TextComponent(String.format("\n          %s%sServer:%s  %s          \n\n", ChatColor.YELLOW,
-                        ChatColor.BOLD, ChatColor.GRAY,
-                        postLoginEvent.getPlayer().getServer().getInfo().getName()
-                ))
-        );
+    public void onPostLogin(PostLoginEvent event) {
+        ProxyServer.getInstance().broadcast(new TextComponent(event.getPlayer().getName() + " has joined the network!"));
+        getLogger().info(event.getPlayer().getName() + " joined mc.nevah5.dev");
     }
 }
